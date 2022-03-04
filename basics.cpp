e5ff045384568ca8f51dd3996c55dc29e91ec82c
Will <twitchifygamers@gmail.com>
2022-03-04 14:57:59 +0000

    Initial commit plus basic functions

---
+++ basics.cpp
@@ -0,0 +1,21 @@
+//
+// Created by will on 04/03/2022.
+//
+
+#include "headers/basics.h"
+
+int add() {
+
+}
+
+int subtract() {
+
+}
+
+int divide() {
+
+}
+
+int multiply() {
+
+}
---
+++ headers/basics.h
@@ -0,0 +1,12 @@
+//
+// Created by will on 04/03/2022.
+//
+
+#ifndef MATICA_BASICS_H
+#define MATICA_BASICS_H
+
+namespace basic {
+
+}
+
+#endif //MATICA_BASICS_H
